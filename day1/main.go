e2babd807762399fb23fdc95e97be18de2d1f9dc
vpreseault <vincent.preseault@gmail.com>
2024-12-01 17:41:52 -0500

    day1.1

--- day1/main.go
+++ day1/main.go
@@ -1,1 +1,69 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	data, err := os.ReadFile("./day1/input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	lines := strings.Split(string(data), "\n")
+	leftList, rightList := []int{}, []int{}
+	for _, line := range lines {
+		distances := strings.Split(line, "   ")
+		if leftDistance, err := strconv.Atoi(distances[0]); err == nil {
+			leftList = append(leftList, leftDistance)
+		}
+		if rightDistance, err := strconv.Atoi(distances[1]); err == nil {
+			rightList = append(rightList, rightDistance)
+		}
+	}
+
+	leftList = quicksort(leftList)
+	rightList = quicksort(rightList)
+	fmt.Println(rightList)
+
+	totalDistance := 0
+	for i := 0; i < len(leftList); i++ {
+		distance := leftList[i] - rightList[i]
+		if distance < 0 {
+			distance = -distance
+		}
+		totalDistance += distance
+	}
+
+	fmt.Println(totalDistance)
+}
+
+func quicksort(arr []int) []int {
+	if len(arr) < 2 {
+		return arr
+	}
+
+	left, right := 0, len(arr)-1
+
+	pivot := len(arr) / 2
+
+	arr[pivot], arr[right] = arr[right], arr[pivot]
+
+	for i := range arr {
+		if arr[i] < arr[right] {
+			arr[i], arr[left] = arr[left], arr[i]
+			left++
+		}
+	}
+
+	arr[left], arr[right] = arr[right], arr[left]
+
+	quicksort(arr[:left])
+	quicksort(arr[left+1:])
+
+	return arr
+}
