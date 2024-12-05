40c69f501a125435762e070a06a129f0241de258
vpreseault <vincent.preseault@gmail.com>
2024-12-05 07:45:21 -0500

    day2.2 non-recursive incomplete

--- day2/main.go
+++ day2/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"os"
@@ -23,7 +24,7 @@ func part1(data []byte) {
 	lines := strings.Split(string(data), "\n")
 
 	for _, line := range lines {
-		if verifySafety(line) {
+		if val, err := verifyReportSafety(line); err == nil && val {
 			safeCounter++
 		}
 
@@ -32,7 +33,7 @@ func part1(data []byte) {
 	fmt.Println("Safe levels: ", safeCounter)
 }
 
-func verifySafety(line string) bool {
+func verifyReportSafety(line string) (bool, error) {
 	increasing := false
 	entries := strings.Split(line, " ")
 	for i := 0; i < len(entries)-1; i++ {
@@ -46,30 +47,89 @@ func verifySafety(line string) bool {
 			log.Fatal(err)
 		}
 
-		if one == two {
-			return false
-		}
-
-		diff := one - two
-		if i == 0 {
-			if diff < 0 {
-				increasing = true
-			}
-		} else {
-			if (diff < 0 && !increasing) || (diff > 0 && increasing) {
-				return false
-			}
-		}
-
-		if abs(diff) > 3 {
-			return false
+		if val, err := verifyLevelSafety(i, &increasing, one, two); !val {
+			return false, err
 		}
 	}
-	return true
+	return true, nil
 }
 
 func part2(data []byte) {
+	safeCounter := 0
+	lines := strings.Split(string(data), "\n")
 
+	for _, line := range lines {
+		if val, err := verifyReportSafetyWithBuffer(line); err == nil && val {
+			safeCounter++
+		}
+
+	}
+
+	fmt.Println("Safe levels with buffer: ", safeCounter)
+}
+
+func verifyReportSafetyWithBuffer(line string) (bool, []error) {
+	increasing := false
+	bufferUsed := false
+	var errs []error
+	entries := strings.Split(line, " ")
+	for i := 0; i < len(entries)-1; i++ {
+		one, err := strconv.Atoi(entries[i])
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		two, err := strconv.Atoi(entries[i+1])
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if val, err := verifyLevelSafety(i, &increasing, one, two); !val {
+			errs = append(errs, err)
+			if i+2 < len(entries) && !bufferUsed {
+				three, err := strconv.Atoi(entries[i+2])
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				bufferUsed = true
+				if val, err := verifyLevelSafety(i, &increasing, one, three); !val {
+					errs = append(errs, err)
+					return false, errs
+				}
+				i++ // skip two
+			}
+		}
+
+	}
+	return true, errs
+}
+
+func verifyLevelSafety(index int, increasing *bool, n1, n2 int) (bool, error) {
+	if n1 == n2 {
+		return false, errors.New("duplicate values")
+	}
+
+	diff := n1 - n2
+	if index == 0 {
+		if diff < 0 {
+			*increasing = true
+		}
+	} else {
+		if (diff < 0 && !*increasing) || (diff > 0 && *increasing) {
+			if *increasing {
+				return false, fmt.Errorf("wanted to increase, but decreased")
+			} else {
+				return false, fmt.Errorf("wanted to decrease, but increased")
+			}
+
+		}
+	}
+
+	if abs(diff) > 3 {
+		return false, fmt.Errorf("difference is greater than 3")
+	}
+	return true, nil
 }
 
 func abs(n int) int {
--- day2/main_test.go
+++ day2/main_test.go
@@ -2,79 +2,68 @@ package main
 
 import "testing"
 
-func TestVerifySafety(t *testing.T) {
+func TestVerifyReportSafety(t *testing.T) {
 	tc := []struct {
 		line     string
 		expected bool
 	}{
-		{
-			line:     "24 25 28 31 28",
-			expected: false,
-		},
-		{
-			line:     "41 44 45 48 49 50 50",
-			expected: false,
-		},
-		{
-			line:     "5 8 10 13 15 16 17 21",
-			expected: false,
-		},
-		{
-			line:     "11 13 16 17 19 26",
-			expected: false,
-		},
-		{
-			line:     "79 81 78 79 82 84",
-			expected: false,
-		},
-		{
-			line:     "16 19 20 18 20 22 25 22",
-			expected: false,
-		},
-		{
-			line:     "84 87 90 92 94 97 96 96",
-			expected: false,
-		},
-		{
-			line:     "86 87 88 91 88 91 95",
-			expected: false,
-		},
-		{
-			line:     "40 43 41 44 49",
-			expected: false,
-		},
-		{
-			line:     "8 10 10 11 13",
-			expected: false,
-		},
-		{
-			line:     "91 94 95 95 92",
-			expected: false,
-		},
-		{
-			line:     "18 19 20 21 23 25",
-			expected: true,
-		},
-		{
-			line:     "15 17 19 20 23",
-			expected: true,
-		},
-		{
-			line:     "35 37 37 39 40 43 50",
-			expected: false,
-		},
-		{
-			line:     "2 5 6 10 12",
-			expected: false,
-		},
+		{"7 6 4 2 1", true},
+		{"1 2 7 8 9", false},
+		{"9 7 6 2 1", false},
+		{"1 3 2 4 5", false},
+		{"8 6 4 4 1", false},
+		{"1 3 6 7 9", true},
 	}
 
 	for _, tt := range tc {
 		tt := tt
 		t.Run(tt.line, func(t *testing.T) {
-			actual := verifySafety(tt.line)
+			actual, err := verifyReportSafety(tt.line)
 			if actual != tt.expected {
-				t.Errorf("Expected %v, but got %v for line: %v", tt.expected, actual, tt.line)
+				t.Errorf("Expected %v, but got [%v, %v] for line: %v", tt.expected, actual, err, tt.line)
+			}
+		})
+	}
+}
+
+func TestVerifyReportSafetyWithBuffer(t *testing.T) {
+	tc := []struct {
+		line     string
+		expected bool
+		err      error
+	}{
+		{"7 6 4 2 1", true, nil},
+		{"1 2 7 8 9", false, nil},
+		{"9 7 6 2 1", false, nil},
+		{"1 3 2 4 5", true, nil},
+		{"8 6 4 4 1", true, nil},
+		{"1 3 6 7 9", true, nil},
+		{"24 25 28 31 28", true, nil},
+		{"41 44 45 48 49 50 50", true, nil},
+		{"5 8 10 13 15 16 17 21", true, nil},
+		{"11 13 16 17 19 26", true, nil},
+		{"79 81 78 79 82 84", false, nil},
+		{"16 19 20 18 20 22 25 22", false, nil},
+		{"84 87 90 92 94 97 96 96", false, nil},
+		{"86 87 88 91 88 91 95", false, nil},
+		{"8 10 10 11 13", true, nil},
+
+		{"84 87 90 92 94 96 96", true, nil},
+		{"84 87 90 92 94 96 101", true, nil},
+
+		{"80 87 90 92 94 96", true, nil},
+		{"80 87 90 92 94 96 101", false, nil},
+
+		{"40 43 41 44 49", false, nil},
+		{"40 44 41 40 37", true, nil},
+	}
+
+	for _, tt := range tc {
+		tt := tt
+		t.Run(tt.line, func(t *testing.T) {
+			actual, err := verifyReportSafetyWithBuffer(tt.line)
+			if actual != tt.expected {
+				t.Errorf("Expected %v, but got [%v, [%v,%v]] for line: %v", tt.expected, actual, err[0], err[1], tt.line)
 			}
 		})
 	}
